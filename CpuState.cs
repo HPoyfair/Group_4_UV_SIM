cded046e2bab3bcd30393f4485d982212b3cb57b
HPoyfair <haydenpoyfair@gmail.com>
2026-02-07 13:07:44 -0700

    Clean project structure and remove duplicates

--- CpuState.cs
+++
@@ -1,9 +0,0 @@
-
-namespace Group_4_UV_SIM;
-public class CpuState
-{
-    public int[] Memory { get; } = new int[100];
-    public int Accumulator { get; set; }
-    public int InstructionPointer { get; set; }
-    public bool Halted { get; set; }
-}
