008df929dea3fc6d2f7aad2a796804b820f6357f
Jason <jasonodavis323@gmail.com>
2026-02-07 14:16:25 -0700

    input output tests done, check read and write

--- Group_4_UV_SIM.Tests/InputOutputTests.cs
+++ Group_4_UV_SIM.Tests/InputOutputTests.cs
@@ -1,35 +1,85 @@
 using Xunit;
+using System;
+using System.IO;
 using Group_4_UV_SIM; // <- your namespace
 
 
 public class InputOutputTests
 {
     [Fact]
-    public void Input_ReadsValueIntoAccumulator()
+    public void Input_ReadsValueIntoMemory()
     {
             var cpu = new CpuState();
             cpu.InstructionPointer = 5;
     
             // Simulate input by directly setting the value in memory
-            cpu.Memory[10] = 42; // Let's say the input instruction reads from address 10
-    
+           
+            Console.SetIn(new StringReader("42")); 
             InputOutput.Read(30, 10, cpu);
     
-            Assert.Equal(42, cpu.Accumulator);
+            Assert.Equal(42, cpu.Memory[10]);
             Assert.Equal(6, cpu.InstructionPointer);
     }
 
     [Fact]
-    public void Output_WritesAccumulatorToMemory()
+public void Read_InvalidThenValidInput_RepromptsAndStoresValidValue()
+{
+    var cpu = new CpuState();
+    cpu.InstructionPointer = 5;
+
+    var originalIn = Console.In;
+    var originalOut = Console.Out;
+
+    try
     {
-            var cpu = new CpuState();
-            cpu.Accumulator = 99;
-            cpu.InstructionPointer = 5;
-    
-            InputOutput.Write(31, 20, cpu);
-    
-            Assert.Equal(99, cpu.Memory[20]);
-            Assert.Equal(6, cpu.InstructionPointer);
-       
+        // first line invalid (too big), second line valid
+        Console.SetIn(new StringReader("123456\n42\n"));
+        Console.SetOut(new StringWriter()); // capture/suppress spam
+
+        InputOutput.Read(10, 10, cpu);
+
+        Assert.Equal(42, cpu.Memory[10]);
+        Assert.Equal(6, cpu.InstructionPointer);
     }
+    finally
+    {
+        Console.SetIn(originalIn);
+        Console.SetOut(originalOut);
+    }
+}
+
+    [Fact]
+public void Write_PrintsMemoryValue_AndAdvancesIP()
+{
+    var cpu = new CpuState();
+    cpu.InstructionPointer = 5;
+
+    // ensure address is valid
+    Assert.True(cpu.Memory.Length > 20);
+
+    cpu.Memory[20] = 99;
+
+    var originalOut = Console.Out;
+
+    try
+    {
+        var sw = new StringWriter();
+        Console.SetOut(sw);
+
+        InputOutput.Write(11, 20, cpu);
+
+        var output = sw.ToString();
+
+        // check formatted output
+        Assert.Contains("memory[20] = +0099", output);
+
+        // check IP increment
+        Assert.Equal(6, cpu.InstructionPointer);
+    }
+    finally
+    {
+        Console.SetOut(originalOut);
+    }
+}
+
 }
--- Group_4_UV_SIM/obj/Debug/net8.0/Group_4_UV_SIM.AssemblyInfo.cs
+++ Group_4_UV_SIM/obj/Debug/net8.0/Group_4_UV_SIM.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("Group_4_UV_SIM")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+20a6b8c271f51a6b35f3db97bd157ffd6597f46c")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+f14b82381b0ad82e90dc86bd4da04ab5e9078889")]
 [assembly: System.Reflection.AssemblyProductAttribute("Group_4_UV_SIM")]
 [assembly: System.Reflection.AssemblyTitleAttribute("Group_4_UV_SIM")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
