9053a9901eca303d841c4f502d07410d65bb8d21
Joshua England <joshua.england1@students.snow.edu>
2026-04-10 19:53:43 -0600

    Changes to comments

--- Group_4_UV_SIM/CpuState.cs
+++ Group_4_UV_SIM/CpuState.cs
@@ -2,7 +2,7 @@ namespace Group_4_UV_SIM;
 
 public class CpuState
 {
-    // this breaks the ui currently!!!!! see form 1 to fix it and adjust size of actual gui in designer.cs
+
     public int[] Memory { get; } = new int[250];
     public int Accumulator { get; set; }
     public int InstructionPointer { get; set; }
